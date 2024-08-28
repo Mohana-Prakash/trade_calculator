54dab6d39fd3dbc59aaadbcb34b8d7837ccdf195
Mohana Prakash <mohan@ggexcel.ai>
2024-08-28 18:03:28 +0530

    meter changes

--- src/components/radiusMeter.js
+++ src/components/radiusMeter.js
@@ -1,15 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import moment from "moment";
 
 function RadiusMeter() {
   const [content, setContent] = useState([]);
+  const [date, setDate] = useState("");
+
+  const arr = [
+    "D/T",
+    "",
+    "",
+    "",
+    "V1",
+    "V2",
+    "V3",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ];
+
   useEffect(() => {
     const postData = async () => {
       const url = "https://sm3.talk2device.com/gtDta";
       const reqObj = {
         type: "LP",
         meter_serial_no: "22053241",
-        date: "2024-08-27",
+        date: moment(date).format("YYYY-MM-DD"),
         count: 0,
         r_count: 100,
       };
@@ -25,17 +59,36 @@ function RadiusMeter() {
     };
 
     postData();
-  }, []);
+  }, [date]);
 
   return (
     <div style={{ marginTop: "10px" }}>
-      <span>Records: <b>{content.length}</b></span>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "10px",
+        }}>
+        <span>
+          Records: <b>{content.length}</b>
+        </span>
+        <input
+          type="date"
+          name="date"
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
       <table>
-        {content.map((e) => {
+        <tr>
+          {arr.map((e, i) => {
+            return <th key={i}>{e}</th>;
+          })}
+        </tr>
+        {content.map((e, i) => {
           return (
-            <tr key={e}>
-              {e.split(",").map((a, i) => {
-                return <td key={a}>{i === 0 ? a : Number(a).toFixed(2)}</td>;
+            <tr key={i}>
+              {e.split(",").map((a, b) => {
+                return <td key={b}>{b === 0 ? a : Number(a).toFixed(2)}</td>;
               })}
             </tr>
           );
